09c266b4235da8023402dac3cc7b04d0ec4d22c2
Marcelo Daniel <danielmarcelo83@gmail.com>
2019-10-31 17:31:43 -0300

    Create Clase 3 ejercicio1.js
    /* 1 Un hipódromo necesita un programa que haga una simple cuentaregresiva desde un número entero positivo introducido por el usuario hasta el 0. El programa deberá mostrar cada uno de los números de forma descendente excepto por el último donde se reemplazará por la palabra “Largaron”. */

---
+++ Clase 3 ejercicio1.js
@@ -0,0 +1,13 @@
+/* 1 Un hipódromo necesita un programa que haga una simple cuenta
+regresiva desde un número entero positivo introducido por el usuario
+hasta el 0. El programa deberá mostrar cada uno de los números de
+forma descendente excepto por el último donde se reemplazará por la
+palabra “Largaron”. */
+
+for(var i=10; i<=10;i--){
+ console.log(i)
+if(i==1){
+    console.log('¡Largaron!')
+    break;
+}
+}
